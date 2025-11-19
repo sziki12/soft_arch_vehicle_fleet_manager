fd649b96acd89e7083718521199fd19d369b5e38
andris0409 <andrisvajda71@gmail.com>
2025-11-19 14:56:52 +0100

    Add initial components and their corresponding styles and tests

---
+++ Frontend/SoftArchVeichleManager/src/app/manufecturer/module-form/module-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module-form',
+  imports: [],
+  templateUrl: './module-form.component.html',
+  styleUrl: './module-form.component.css'
+})
+export class ModuleFormComponent {
+
+}
