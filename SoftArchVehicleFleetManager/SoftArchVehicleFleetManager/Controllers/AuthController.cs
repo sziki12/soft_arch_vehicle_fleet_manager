8a65838dbd9f33ddf026f9ab924dd94ded0f177f
Bence Zsolt Pinter <piben111@gmail.com>
2025-11-30 14:14:55 +0100

    auth/register endpoint

--- SoftArchVehicleFleetManager/SoftArchVehicleFleetManager/Controllers/AuthController.cs
+++ SoftArchVehicleFleetManager/SoftArchVehicleFleetManager/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
 using SoftArchVehicleFleetManager.Services;
+using SoftArchVehicleFleetManager.Enums;
 
 [ApiController]
 [Route("auth")]
@@ -28,10 +29,30 @@ public class AuthController : ControllerBase
             _ => StatusCode(StatusCodes.Status500InternalServerError)
         };
     }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var result = await _authService.RegisterAsync(request.Username, request.Role, request.Password);
+
+        return result.Status switch
+        {
+            RegisterResultStatus.Success => Ok(new { token = result.Token }),
+            RegisterResultStatus.UsernameAlreadyExists => BadRequest(new { error = "Username already exists." }),
+            _ => StatusCode(StatusCodes.Status500InternalServerError)
+        };
+    }
 }
 
 public record LoginRequest
 (
     [property: JsonPropertyName("USERNAME")] string Username,
+    [property: JsonPropertyName("PASSWORD")] string Password
+);
+
+public record RegisterRequest
+(
+    [property: JsonPropertyName("USERNAME")] string Username,
+    [property: JsonPropertyName("USER_ROLE")] UserRole Role,
     [property: JsonPropertyName("PASSWORD")] string Password
 );
